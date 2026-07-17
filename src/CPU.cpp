cea6df68a281e380388171ed0d7595c89dccf4ac
Nicolas Padilla <padillanico2016@gmail.com>
2026-07-16 17:27:51 -0700

    Implement CPU accessor layer: constructor, register + flag get/set

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -1,3 +1,23 @@
 #include "CPU.h"
 
-//TODO
+CPU::CPU() {
+    // load standard initial register values for an original Game Boy (DMG) -- from PanDocs
+    _a = 0x01;
+    _f = 0xB0;
+    _b = 0x00;
+    _c = 0x13;
+    _d = 0x00;
+    _e = 0xD8;
+    _h = 0x01;
+    _l = 0x4D;
+    _sp = 0xFFFE; //top of HRAM
+    _pc = 0x0100; //execution entry point on game cartridge
+}
+
+uint8_t CPU::getA() const {
+    return _a;
+}
+uint8_t CPU::getF() const {
+    return _f;
+}
+uint8_t CPU::get
